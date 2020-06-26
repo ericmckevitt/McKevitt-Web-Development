0c090acc84e4bf266b0c4661b157c45538a58cb5
ericmckevitt <64185510+ericmckevitt@users.noreply.github.com>
2020-06-26 17:10:01 -0500

    fourth commit

---
+++ js/navsection.js
@@ -0,0 +1,39 @@
+function switchNav() {
+    var navHeight = "360px";
+    var navSectionContainerHeight = "75px";
+    //var contentsDisplay = "show";
+    var elem = document.getElementById("navsection");
+
+    //important one
+    var navItem1 = document.getElementById("navsectionItem1");
+    var navItem2 = document.getElementById("navsectionItem2");
+    var navItem3 = document.getElementById("navsectionItem3");
+    var navItem4 = document.getElementById("navsectionItem4");
+
+
+    //CONDITIONS
+
+    //if extended
+    if ( elem.style.height == navHeight) {
+        //retract
+        navHeight = "120px";
+    }
+
+    //DO THINGS
+    elem.style.height = navHeight;
+
+    if (navHeight !== "120px") {
+        //EXPAND
+        document.getElementById("navsectionItem1").innerHTML = "About";
+        document.getElementById("navsectionItem2").innerHTML = "Portfolio";
+        document.getElementById("navsectionItem3").innerHTML = "Programming";
+        document.getElementById("navsectionItem4").innerHTML = "Contact";
+    } else {
+        //RETRACT
+        document.getElementById("navsectionItem1").innerHTML = "";
+        document.getElementById("navsectionItem2").innerHTML = "";
+        document.getElementById("navsectionItem3").innerHTML = "";
+        document.getElementById("navsectionItem4").innerHTML = "";
+    }
+    
+}
